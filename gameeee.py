e3c6b414fb12d5ad2feb4e189bec2b33cf84444a
kitreees <andimay092000@gmail.com>
2023-08-21 12:15:17 +0300

    рабочие вопросы

--- gameeee.py
+++ gameeee.py
@@ -31,11 +31,15 @@ kpop_q1 = False
 anime_q1 = False
 kpop_q2 = False
 anime_q2 = False
+kpop_q3 = False
+anime_q3 = False
 
 # bg_k = pygame.image.load('for game/bg_k.jpg')
 # bg_a = pygame.image.load('for game/bg_a.jpg')
 
 kpop_q1_img = pygame.image.load('for game/gidle.png')
+kpop_q2_img = pygame.image.load('for game/enha.jpg')
+kpop_q3_img = pygame.image.load('for game/g-idle.png')
 
 txt = pygame.image.load('for game/txt.png')
 txt_rect = txt.get_rect()
@@ -57,31 +61,82 @@ gidle_rect = gidle.get_rect()
 gidle_rect.x = 680
 gidle_rect.y = 440
 
-score = 0
-lose = False
+enhypen = pygame.image.load('for game/enhypen.png')
+enhypen_rect = enhypen.get_rect()
+enhypen_rect.x = 680
+enhypen_rect.y = 440
+
+nmixx = pygame.image.load('for game/nmixx.png')
+nmixx_rect = nmixx.get_rect()
+nmixx_rect.x = 90
+nmixx_rect.y = 440
+
+yena = pygame.image.load('for game/yena.png')
+yena_rect = yena.get_rect()
+yena_rect.x = 680
+yena_rect.y = 580
+
+itzy = pygame.image.load('for game/itzy.png')
+itzy_rect = itzy.get_rect()
+itzy_rect.x = 90
+itzy_rect.y = 580
+
+sc0 = pygame.image.load('for game/0.png')
+sc10 = pygame.image.load('for game/10.png')
+sc20 = pygame.image.load('for game/20.png')
+
+
+lose1k = False
+lose2k = False
+lose3k = False
+
 
 # font = pygame.font.Font(None, 36)
 
 
 def Kpop():
-
     global bg
     bg = pygame.image.load('for game/dudu.jpg')
-
 
 
 def Anime():
     global bg
     bg = pygame.image.load('for game/bg_a.jpg')
 
-def f_sc():
 
+def f_sc():
     global bg
     bg = pygame.image.load('for game/dddd.jpg')
+    sc.blit(bg, (0, 0))
+
+
+def kpop_q1_f():
+    sc.blit(bg, (0, 0))
+    sc.blit(kpop_q1_img, (350, 50))
+    sc.blit(txt, txt_rect)
+    sc.blit(bts, bts_rect)
+    sc.blit(twice, twice_rect)
+    sc.blit(gidle, gidle_rect)
+
+
+def kpop_q2_f():
+    sc.blit(bg, (0, 0))
+    sc.blit(kpop_q2_img, (350, 50))
+    sc.blit(txt, txt_rect)
+    sc.blit(bts, bts_rect)
+    sc.blit(twice, twice_rect)
+    sc.blit(gidle, gidle_rect)
+
+def kpop_q3_f():
+    sc.blit(bg, (0, 0))
+    sc.blit(kpop_q3_img, (350, 50))
+    sc.blit(txt, txt_rect)
+    sc.blit(bts, bts_rect)
+    sc.blit(twice, twice_rect)
+    sc.blit(gidle, gidle_rect)
 
 
 def finish():
-
     # global bg
     # bg = pygame.image.load('for game/bg.jpg')
     #
@@ -96,11 +151,12 @@ def finish():
     pygame.quit()
     sys.exit()
 
+
 def main():
-    global main_screen, kpop_screen, anime_screen, kpop_q1, anime_q1, kpop_q2, anime_q2, score, lose
+    global main_screen, kpop_screen, anime_screen, kpop_q1, anime_q1, kpop_q2, anime_q2, kpop_q3, anime_q3, score, lose1k, lose2k
+    score = 0
     x = 0
     y = 0
-
 
     while True:
         pygame.mouse.set_visible(False)
@@ -110,67 +166,125 @@ def main():
                 finish()
             elif event.type == pygame.MOUSEMOTION:
                 x, y = event.pos
-            elif event.type == pygame.MOUSEBUTTONDOWN:
+            elif event.type == pygame.MOUSEBUTTONDOWN and main_screen:
                 if event.button == 1:
                     tap = pygame.Rect(x, y, 1, 1)
                     if tap.colliderect(kpop_rect):
                         main_screen = False
-                        kpop_screen = True
+                        kpop_q1 = True
                         anime_screen = False
                         Kpop()
                     elif tap.colliderect(anime_rect):
                         main_screen = False
-                        kpop_screen = False
+                        kpop_q1 = False
                         anime_screen = True
                         Anime()
-
+            elif event.type == pygame.MOUSEBUTTONDOWN and kpop_q1:
+                if event.button == 1:
+                    tap = pygame.Rect(x, y, 1, 1)
+                    if tap.colliderect(gidle_rect):
+                        kpop_q1 = False
+                        score += 10
+                        kpop_q2 = True
+                    elif tap.colliderect(twice_rect):
+                        kpop_q1 = False
+                        kpop_q2 = False
+                        lose1k = True
+                        f_sc()
+                    elif tap.colliderect(bts_rect):
+                        kpop_q1 = False
+                        kpop_q2 = False
+                        lose1k = True
+                        f_sc()
+                    elif tap.colliderect(txt_rect):
+                        kpop_q1 = False
+                        kpop_q2 = False
+                        lose1k = True
+                        f_sc()
+            elif event.type == pygame.MOUSEBUTTONDOWN and kpop_q2:
+                if event.button == 1:
+                    tap = pygame.Rect(x, y, 1, 1)
+                    if tap.colliderect(gidle_rect):
+                        score += 10
+                        kpop_q2 = False
+                        kpop_q3 = True
+                    elif tap.colliderect(twice_rect):
+                        kpop_q2 = False
+                        kpop_q3 = False
+                        lose1k = True
+                        f_sc()
+                    elif tap.colliderect(bts_rect):
+                        kpop_q2 = False
+                        kpop_q3 = False
+                        lose1k = True
+                        f_sc()
+                    elif tap.colliderect(txt_rect):
+                        kpop_q2 = False
+                        kpop_q3 = False
+                        lose1k = True
+                        f_sc()
+            elif event.type == pygame.MOUSEBUTTONDOWN and kpop_q3:
+                if event.button == 1:
+                    tap = pygame.Rect(x, y, 1, 1)
+                    if tap.colliderect(gidle_rect):
+                        score += 1
+                        kpop_q3 = False
+                        kpop_q4 = True
+                    elif tap.colliderect(twice_rect):
+                        kpop_q3 = False
+                        kpop_q4 = False
+                        lose2k = True
+                        f_sc()
+                    elif tap.colliderect(bts_rect):
+                        kpop_q3 = False
+                        kpop_q4 = False
+                        lose2k = True
+                        f_sc()
+                    elif tap.colliderect(txt_rect):
+                        kpop_q3 = False
+                        kpop_q4 = False
+                        lose2k = True
+                        f_sc()
 
         if main_screen:
             sc.blit(bg, (0, 0))
             sc.blit(text, (350, 70))
             sc.blit(kpop, kpop_rect)
             sc.blit(anime, anime_rect)
-
-
-        if kpop_screen:
-            sc.blit(bg, (0, 0))
-            kpop_q1 = True
-
+        #
+        # if kpop_screen:
+        #     sc.blit(bg, (0, 0))
+        #     kpop_q1 = True
 
         if anime_screen:
             sc.blit(bg, (0, 0))
             anime_q1 = True
 
+        if lose1k:
+            f_sc()
+            sc.blit(sc0, (350, 70))
+
+        if lose2k:
+            f_sc()
+            sc.blit(sc10, (350, 70))
+
+        if lose3k:
+            f_sc()
+            sc.blit(sc20, (350, 70))
 
         if kpop_q1:
-            sc.blit(kpop_q1_img, (350, 50))
-            sc.blit(txt, txt_rect)
-            sc.blit(bts, bts_rect)
-            sc.blit(twice, twice_rect)
-            sc.blit(gidle, gidle_rect)
-            if tap.colliderect(gidle_rect):
-                score += 1
-                kpop_q2 = True
-            elif tap.colliderect(twice_rect):
-                lose = True
-            elif tap.colliderect(bts_rect):
-                lose = True
-            elif tap.colliderect(txt_rect):
-                lose = True
+            kpop_q1_f()
 
         if kpop_q2:
-            sc.blit(bg, (0, 0))
-            sc.blit(kpop_q1_img, (350, 50))
+            kpop_q2_f()
 
-        if lose and f_sc():
-            sc.blit(bg, (0, 0))
-
+        if kpop_q3:
+            kpop_q3_f()
 
         sc.blit(kursor, (x, y))
 
         clock.tick(FPS)
         pygame.display.update()
-
 
 
 main()
